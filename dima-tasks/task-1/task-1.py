7cd32dcb648529724b70d843cc26f46ec420c717
andrey <drobahandrey7@gmail.com>
2025-03-02 16:31:28 +0500

    dima tasks : directory 'dima-tasks' created in the root :
    - notes-rus.txt file added;
    - task-1 directory added, it's the first task;

---
+++ dima-tasks/task-1/task-1.py
@@ -0,0 +1,20 @@
+"""
+Следующие две строки нужны для запуска этого скрипта
+в терминале из текущей директории.
+Это связано с тем, что модуль core находится
+в другой директории (не в текущей).
+"""
+import sys
+sys.path.append( "../../" )
+
+from src.core import UserAccount
+
+
+def print_user_account( user_account : UserAccount ) :
+    pass
+
+def task_1a() :
+    pass
+
+def task_1b() :
+    pass
