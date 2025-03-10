d450b20dad215f9634986a452eb629a419099375
Hakoho <datyev2022@gmail.com>
2025-03-10 20:44:45 +0300

    exercise 1_c completed

--- dima-tasks/task-1/task-1.py
+++ dima-tasks/task-1/task-1.py
@@ -5,16 +5,18 @@
 в другой директории (не в текущей).
 """
 import sys
-sys.path.append( "../../" )
+
+sys.path.append("../../")
 
 from src.core import UserAccount
+from task_1_helper import TestCase, run_tests
 
 
-def print_user_account(person) :
+def print_user_account(person):
     print(f'username - {person.username}, password - {person.password}')
 
 
-def task_1a(x, y) :
+def task_1a(x, y):
     psn = UserAccount(x, y)
     print_user_account(psn)
 
@@ -22,10 +24,25 @@ def task_1a(x, y) :
 task_1a('DM', '888')
 
 
-def task_1b(account_lst) :
+def task_1b(account_lst):
     for i in account_lst:
         print_user_account(i)
 
 
-task_1b( [(UserAccount( i, i+100)) for i in range(1, 7)] )
+task_1b([(UserAccount(i, i + 100)) for i in range(1, 7)])
 
+
+def is_normal_username(username):
+    if username[0].isalpha() and len(username) >= 3:
+        return True if username.isalpha() or username.isalnum() else False
+    return False
+
+
+s = TestCase('dggdgd', True)
+n = TestCase('1ffhfj', False)
+o = TestCase('DA', False)
+w = TestCase('d**ghghg', False)
+u = TestCase('&fghjjd', False)
+p = TestCase('ABRAKADABRA', True)
+
+run_tests(is_normal_username, [s, n, o, w, u, p])
