dcc7fb8a9e2f5d09c71c7637c9d6d7041452eaf4
andrey <drobahandrey7@gmail.com>
2025-04-03 16:00:01 +0500

    dima-tasks : task 2 added

---
+++ dima-tasks/task-2/task_2.py
@@ -0,0 +1,15 @@
+"""
+Следующие две строки нужны для запуска этого скрипта
+в терминале из текущей директории.
+"""
+import sys
+sys.path.append( "../../" )
+sys.path.append( "../../src/" )
+
+from src import core
+from src.server import Server
+from src.dispatcher import Dispatcher
+
+
+def task_2a() :
+    pass
